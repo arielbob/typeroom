c20c50e4f463d34523da257ce29e8e2b637b2de1
Ariel Bobadilla <arielbdilla@gmail.com>
2018-12-26 16:03:21 -0500

    Added createRoom function

--- roomData.js
+++ roomData.js
@@ -4,17 +4,35 @@ let rooms = {
     text,
     wordArray: text.split(' '),
     numWinners: 0,
+    playerIds: [],
     playersById: {}
   },
   1: {
     text: 'This is the text from room 1',
     wordArray: 'This is the text from room 1'.split(' '),
     numWinners: 0,
+    playerIds: [],
     playersById: {}
   }
 }
 
 let numRooms = 2
+
+const createRoom = () => {
+  const id = numRooms
+  const text = 'This is the text from room ' + id
+
+  rooms[id] = {
+    text,
+    wordArray: text.split(' '),
+    numWinners: 0,
+    playerIds: [],
+    playersById: {}
+  }
+
+  numRooms++
+  return id
+}
 
 const resetRoom = (id) => {
   if (rooms.hasOwnProperty(id)) {
@@ -25,6 +43,7 @@ const resetRoom = (id) => {
 
 const roomData = {
   rooms,
+  createRoom,
   resetRoom,
   numRooms
 }
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,18 +1,9 @@
 const router = require('express').Router()
 const roomData = require('../roomData')
-const { rooms } = roomData
+const { rooms, createRoom } = roomData
 
 router.post('/create', (req, res) => {
-  const roomId = roomData.numRooms++
-  const text = 'Type this text from room ' + roomId
-
-  rooms[roomId] = {
-    text,
-    wordArray: text.split(' '),
-    numWinners: 0,
-    playersById: {}
-  }
-
+  const roomId = createRoom()
   console.log('Room created:', rooms[roomId])
 
   res.redirect('/room/' + roomId)

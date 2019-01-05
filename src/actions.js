1becebbfdfc670c04475aecbf72ab4a398ce99db
Ariel Bobadilla <arielbdilla@gmail.com>
2019-01-05 16:19:11 -0500

    Add word checking on client

--- src/actions.js
+++ src/actions.js
@@ -23,25 +23,35 @@ export const leaveRoom = () => ({
   type: 'LEAVE_ROOM'
 })
 
-// TODO: we should check if the word is correct and update the player's currentWord in here.
-// we don't check for wins or actual progress; only if it matches the game text. this is so that
-// we don't have to wait for the server to update the player's word id and so we keep the typing
-// feel responsive
-export const setInputValue = (inputValue) => (dispatch) => {
+export const setInputValue = (inputValue) => (dispatch, getState) => {
   const spaceEntered = inputValue.charAt(inputValue.length - 1) === ' '
 
   if (spaceEntered) {
-    if (inputValue.trim().length > 0) {
-      // this.props.socket.emit('word input', inputValue)
+    const trimmedVal = inputValue.trim()
+    if (trimmedVal.length > 0) {
+      // empty the text box when they hit space
       dispatch({
         type: 'SET_INPUT_VALUE',
         payload: { inputValue: '' }
       })
 
+      // check if the word is correct so that we don't have to wait for the server
+      // to send back a nextWordId or not
+      const { gameText, clientId, playersById } = getState()
+      const player = playersById[clientId]
+
+      if (player) {
+        const { nextWordId } = player
+        if (trimmedVal == gameText.split(' ')[nextWordId]) {
+          dispatch(setNextWordId(clientId, nextWordId + 1))
+        }
+      }
+
+      // send the input to the server
       dispatch({
         type: 'INPUT_WORD',
         payload: {
-          word: inputValue.trim()
+          word: trimmedVal
         }
       })
     }
@@ -105,13 +115,23 @@ export const addPlayer = (player) => (dispatch, getState) => {
   })
 }
 
-export const setNextWordId = (id, nextWordId) => ({
-  type: 'SET_NEXT_WORD_ID',
-  payload: {
-    id,
-    nextWordId
+export const setNextWordId = (id, nextWordId) => (dispatch, getState) => {
+  const { clientId, playersById } = getState()
+  const player = playersById[clientId]
+
+  // we check if the received nextWordId is greater since we're tracking the id on the client
+  // as well. we don't want the text highlighting to jerk back if there's some delay in receiving
+  // a message from the server
+  if ((id != clientId) || (player && nextWordId > player.nextWordId)) {
+    dispatch({
+      type: 'SET_NEXT_WORD_ID',
+      payload: {
+        id,
+        nextWordId
+      }
+    })
   }
-})
+}
 
 export const setPlace = (id, place) => ({
   type: 'SET_PLACE',

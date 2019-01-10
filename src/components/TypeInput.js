e02fd9f012e670a4ea83f722c6d020c42dc2bc57
Ariel Bobadilla <arielbdilla@gmail.com>
2019-01-10 15:10:57 -0500

    Add input styles

--- src/components/TypeInput.js
+++ src/components/TypeInput.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const TypeInput = ({ value, handleChange, disabled }) => (
-  <input type='text' value={value} onChange={(e) => handleChange(e.target.value)} disabled={disabled}></input>
+  <input
+    type='text'
+    value={value}
+    onChange={(e) => handleChange(e.target.value)}
+    disabled={disabled}
+    className='game__input'>  
+  </input>
 )
 
 export default TypeInput

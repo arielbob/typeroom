d78aa7df6b9c0e22fd26d6344971521c26e05f6e
Ariel Bobadilla <arielbdilla@gmail.com>
2019-01-15 20:47:35 -0500

    Add focusing on input when race starts

--- src/components/TypeInput.js
+++ src/components/TypeInput.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
-const TypeInput = ({ value, handleChange, disabled }) => (
-  <input
-    type='text'
-    value={value}
-    onChange={(e) => handleChange(e.target.value)}
-    disabled={disabled}
-    className='game__input'>  
-  </input>
-)
+class TypeInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.input = React.createRef()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.disabled && !this.props.disabled) this.input.current.focus()
+  }
+
+  render() {
+    return (
+      <input
+        ref={this.input}
+        type='text'
+        value={this.props.value}
+        onChange={(e) => this.props.handleChange(e.target.value)}
+        disabled={this.props.disabled}
+        className='game__input'>
+      </input>
+    )
+  }
+}
 
 export default TypeInput

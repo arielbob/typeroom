2046663f430312323bf4291ffc7ecca9b9afba54
Ariel Bobadilla <arielbdilla@gmail.com>
2019-01-15 20:46:40 -0500

    Modify calculateWpm to round to 2 decimal places

--- util/calculateWpm.js
+++ util/calculateWpm.js
@@ -1,5 +1,7 @@
+// FIXME: we might need more precision with delta to be more accurate
+// our time might need to be stored in milliseconds or something
 const calculateWpm = (typed, deltaSecs) => {
-  return (typed.trim().length / 5) / (deltaSecs / 60)
+  return ((typed.trim().length / 5) / (deltaSecs / 60)).toFixed(2)
 }
 
 module.exports = calculateWpm
